cbdeaa14d9112e680d6b9fb56c76c690ca5ef12e
Arushi Saxena <97784490+ArushiS17@users.noreply.github.com>
2024-08-01 15:02:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: School/Print 1 To N Without Loop/print-1-to-n-without-loop.java
+++ Difficulty: School/Print 1 To N Without Loop/print-1-to-n-without-loop.java
@@ -46,9 +46,9 @@ class Solution
   public void printNos(int N)
     {
         //Your code here
-        if(N>=1){
+        if(N>0){
             printNos(N-1);
-            System.out.print(N+ " ");
+            System.out.print(N + " ");
         }
     }
 }
